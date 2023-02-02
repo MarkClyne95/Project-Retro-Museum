773096a511f95165a8e819f40b8c30bb2bf38056
MarkClyne95 <99036942+MarkClyne95@users.noreply.github.com>
2023-02-02 18:13:51 +0000

    Cursor Fixes for Matthew

--- Assets/1. Users/3. Matthew Baldwin/Scripts/Cursor Scripts/SC_CursorControls.cs
+++ Assets/1. Users/3. Matthew Baldwin/Scripts/Cursor Scripts/SC_CursorControls.cs
@@ -1,38 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class SC_CursorControls : MonoBehaviour
 {
-    [Header("Game Objects")]
-    [SerializeField] private GameObject cursor;
-    [SerializeField] private GameObject explosion;
+    [Header("Game Objects")] [SerializeField]
+    private Texture2D cursor;
+
+    [SerializeField] private Texture2D explosion;
+    private Vector2 _mousePosition;
 
     public Vector3 uh = new Vector3();
+
     public Vector3 eh = new Vector2();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //this.gameObject.transform.position = (Vector3)Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        uh = (Vector3)Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        uh.z = 0;
-        this.gameObject.transform.position = uh;
-    
+        
     }
 
     //THIS BIT MARK
-    //private void OnControl(InputAction.CallbackContext context)
-    //{
-    //    Debug.Log(context);
-    //}
+    private void OnControl(InputValue ctx)
+    {
+        
+    }
 
     private void OnExplosion()
     {
