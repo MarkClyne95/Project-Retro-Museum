a439ef6a23e6d5bf9267e0ce1ddec9956e1fb83b
DocHaunt <b00367881@studentmail.uws.ac.uk>
2023-02-22 16:49:48 +0000

    Atari Improvements

--- Assets/1. Users/3. Matthew Baldwin/Scripts/Explosion/SC_KillCart.cs
+++ Assets/1. Users/3. Matthew Baldwin/Scripts/Explosion/SC_KillCart.cs
@@ -1,37 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+
+
 
 public class SC_KillCart : MonoBehaviour
-{
-    [SerializeField] private AudioSource atariSFX;
-    [SerializeField] private AudioSource badtariSFX;
-    // Start is called before the first frame update
-    void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("AtariCart"))
+        [SerializeField] private AudioSource atariSFX;
+        [SerializeField] private AudioSource badtariSFX;
+        [SerializeField] private SC_PlayerStats stats;
+        // Start is called before the first frame update
+        void Start()
         {
-            Destroy(collision.gameObject);
-            atariSFX = GameObject.FindGameObjectWithTag("AtariCartSFX").GetComponent<AudioSource>();
-            atariSFX.PlayOneShot(atariSFX.clip, 2f);
+            stats = GameObject.FindGameObjectWithTag("Player").GetComponent<SC_PlayerStats>();
         }
 
-        if (collision.gameObject.CompareTag("BadtariCart"))
+        // Update is called once per frame
+        void Update()
         {
-            Destroy(collision.gameObject);
-            badtariSFX = GameObject.FindGameObjectWithTag("BadtariCartSFX").GetComponent<AudioSource>();
-            badtariSFX.PlayOneShot(badtariSFX.clip, 2f);
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("AtariCart"))
+            {
+                Destroy(collision.gameObject);
+                stats.DecreaseScore();
+                atariSFX = GameObject.FindGameObjectWithTag("AtariCartSFX").GetComponent<AudioSource>();
+                atariSFX.PlayOneShot(atariSFX.clip, 2f);
+            }
+
+            if (collision.gameObject.CompareTag("BadtariCart"))
+            {
+                Destroy(collision.gameObject);
+                stats.IncreaseScore();
+
+                badtariSFX = GameObject.FindGameObjectWithTag("BadtariCartSFX").GetComponent<AudioSource>();
+                badtariSFX.PlayOneShot(badtariSFX.clip, 2f);
+            }
         }
     }
-}
