be1d8ae53b7f80276abde671790d2ae328acd37b
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2023-04-10 14:52:59 +0100

    Added checkpoint system

---
+++ Assets/1. Users/1. Scott Ewing/Checkpoints/Scripts/MovableObject.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+#if ODIN_INSPECTOR_3_1
+
+#endif
+
+#if SE_EVENTSYSTEM
+using System.Diagnostics;
+using ScottEwing.EventSystem;
+#endif
+
+namespace ScottEwing.Checkpoints{
+    public class MovableObject : CheckpointSaveResetObject{
+        [Tooltip("True: On checkpoint reload, velocity reset to value as of when checkpoint was reached. False: On checkpoint reload, velocity reset to 0. ")]
+        [SerializeField] private bool _useVelocity;
+        [SerializeField] private Transform _defaultRespawnTransform;
+
+        protected enum UpdateRespawnTransformType{ UseCurrentTransform, UseCheckpointRespawnTransform}
+        [SerializeField] protected UpdateRespawnTransformType _updateRespawnTransformType = UpdateRespawnTransformType.UseCurrentTransform;
+        
+        private Vector3 _respawnPosition;
+        private Quaternion _respawnRotation;
+        private Vector3 _respawnVelocity;
+        private Vector3 _respawnAngularVelocity;
+        private Rigidbody _rb;
+
+        private void Awake() {
+            _rb = GetComponent<Rigidbody>();
+            var t =  _defaultRespawnTransform ? _defaultRespawnTransform : transform;
+            _respawnPosition = t.position;
+            _respawnRotation = t.rotation;
+            if (_rb != null) {
+                _respawnVelocity = _rb.velocity;
+                _respawnAngularVelocity = _rb.angularVelocity;
+            }
+            else if (_useVelocity){
+                Debug.LogError("Movable Object Has No Rigidbody", this);
+            }
+        }
+
+        protected override void OnCheckpointReached() => SaveLocation(transform.position, transform.rotation);
+        protected override void OnCheckpointReset() => Respawn();
+
+#if SE_EVENTSYSTEM
+        protected override void OnCheckpointReached(CheckpointReachedEvent obj) {
+            if (_updateRespawnTransformType == UpdateRespawnTransformType.UseCurrentTransform) {
+                SaveLocation(transform.position, transform.rotation);
+            }
+            else {
+                SaveLocation(obj.position, obj.rotation);
+            }
+            
+        }
+
+        protected override void OnCheckpointReset(ResetToCheckpointEvent obj) => Respawn();
+#endif
+
+        protected void SaveLocation(Vector3 position, Quaternion rotation) {
+            //var t = transform;
+            _respawnPosition = position;
+            _respawnRotation = rotation;
+            if (_rb != null) {
+                _respawnVelocity = _rb.velocity;
+                _respawnAngularVelocity = _rb.angularVelocity;
+            }
+        }
+
+        public virtual void Respawn() {
+            var t = transform;
+            t.position = _respawnPosition;
+            t.rotation = _respawnRotation;
+            if (_rb == null) return;
+
+            if (_useVelocity) {
+                _rb.velocity = _respawnVelocity;
+                _rb.angularVelocity = _respawnAngularVelocity;
+            }else {
+                //_rb.AddForce(-_rb.velocity, ForceMode.VelocityChange);
+                //_rb.AddTorque(-_rb.angularVelocity, ForceMode.VelocityChange);
+                _rb.velocity = Vector3.zero;
+                _rb.angularVelocity = Vector3.zero;
+            }
+        }
+    }
+}
---
+++ Assets/1. Users/1. Scott Ewing/Checkpoints/Scripts/RespawnableObject.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using ScottEwing.Checkpoints;
+using Unity.VisualScripting;
+using UnityEngine;
+//#if SE_EVENTSYSTEM
+using ScottEwing.EventSystem;
+//#endif
+
+namespace ScottEwing.Checkpoints
+{
+    /// <summary>
+    /// Inherites from Movable Object. Can respawn when checkpoint is reload but can also respawn independently of checkpoints
+    /// </summary>
+    public class RespawnableObject : MovableObject{
+        [Tooltip("The respawn position and rotation will be updated each time a checkpoint reached event is invoked. If false the default transform will be used indefinitely")]
+        [SerializeField] private bool _updateRespawnTransformOnCheckpointReached = false;
+        
+        [Tooltip("If true the base checkpoint reload functionality will remain as well as the new checkpoint independent respawn")]
+        [SerializeField] private bool _respawnOnCheckpointReload = false;
+
+
+        protected override void OnCheckpointReached() {
+            if (_updateRespawnTransformOnCheckpointReached) {
+                base.OnCheckpointReached();
+            }
+        }
+
+        protected override void OnCheckpointReset() {
+            if (_updateRespawnTransformOnCheckpointReached) {
+                base.OnCheckpointReset();
+            }
+        }
+        
+#if SE_EVENTSYSTEM
+        protected override void OnCheckpointReached(CheckpointReachedEvent obj) {
+            if (_updateRespawnTransformOnCheckpointReached) {
+                base.OnCheckpointReached(obj);
+            }
+        }
+
+        protected override void OnCheckpointReset(ResetToCheckpointEvent obj) {
+            if (_updateRespawnTransformOnCheckpointReached) {
+                base.OnCheckpointReset(obj);
+            }
+        }
+#endif
+
+        public override void Respawn() {
+            StartCoroutine(Routine());
+            //--Need to wait a frame because obi can't deactivate ropes during physics update. This was being called during on trigger enter 
+            IEnumerator Routine() {
+                yield return null;
+                
+                base.Respawn();
+                var evt = new ObjectRespawnedEvent {
+                    respawnedObject = gameObject,
+                    childColliders = GetComponentsInChildren<Collider>()
+                };
+                EventManager.Broadcast(evt);
+            }
+        }
+
+        public void SaveLocation(Collider checkpoint) {
+            var transform = checkpoint.transform;
+            SaveLocation(transform.position, transform.rotation);
+        }
+    }
+}
