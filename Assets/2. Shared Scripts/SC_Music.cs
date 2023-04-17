358c903b229257c9c11bb49a86df3cafb3f28996
DocHaunt <b00367881@studentmail.uws.ac.uk>
2023-04-17 14:18:56 +0100

    Added Music to Menu and Museums

---
+++ Assets/2. Shared Scripts/SC_Music.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Music : MonoBehaviour
+{
+    [Header("Audio Stuff")]
+    private AudioSource source;
+    private float volume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        volume = PlayerPrefs.GetFloat("MusicVol");
+
+        source.volume = volume * .1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
